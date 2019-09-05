d9158ac680d38e32f01657629ca51edaee2e63c4
srmccarthy2 <smccarthy@mail.icecube.wisc.edu>
2019-09-05 15:22:08 -0500

    Update SPEarray.py
    updated division of saved files to be more user friendly

--- SPEarray.py
+++ SPEarray.py
@@ -15,6 +15,9 @@ import glob
 #pDOMdata
 SPE_data = []
 weights = []
+energies =[]
+cos_zeniths = []
+area_eff = []
 pDOM_files = glob.glob('/data/user/msilva/metaprojects/Gen2-Scripts/RECO_SPEs/MCReco_pDOM.i3.bz2')
 for item in pDOM_files:
     f = dataio.I3File(item)
@@ -25,24 +28,40 @@ for item in pDOM_files:
             flux = model.flux(MuonGun.depth(muon.pos.z), np.cos(muon.dir.zenith), 1)*model.energy(MuonGun.depth(muon.pos.z), np.cos(muon.dir.zenith), 1, 0, muon.energy)
             weight = flux*frame['MuonEffectiveArea'].value
             weights.append(weight)
-            energy = frame["MCMuon"].energy
-            cos_zenith = np.cos(frame["MCMuon"].dir.zenith)
-            eff_area = frame["MuonEffectiveArea"].value
+            energies.append(weight*frame["MCMuon"].energy)
+            cos_zeniths.append(weight*np.cos(frame["MCMuon"].dir.zenith))
+            area_eff.append(frame["MuonEffectiveArea"].value)
             for DOM, MCPE in frame["I3RecoPulseSeriesMapGen2"]:
                 for SPE in MCPE:
                     DOMstr = str((DOM[0])+ (DOM[1]))
                     DOMs = int(DOMstr)
-                    data = [DOMs,SPE.charge, SPE.time, weight, cos_zenith, eff_area, energy]
+                    data = [DOMs,SPE.charge, SPE.time]
                     SPE_data.append(data)
-file = '/data/user/smccarthy/gen2_files/weighting/pDOM/final/SPEarray.txt'
-np.savetxt(file,SPE_data) 
+                    print(SPE_data)
+
+with open('/data/user/smccarthy/gen2_files/weighting/pDOM/final/SPEarray.txt', 'w') as filehandle:  
+    for listitem in SPE_data:
+        filehandle.write('\n'% listitem)  
+with open('/data/user/smccarthy/gen2_files/weighting/pDOM/final/energies.txt', 'w') as filehandle:  
+    for listitem in energies:
+        filehandle.write('%f\n' % listitem) 
+with open('/data/user/smccarthy/gen2_files/weighting/pDOM/final/cos_zeniths.txt', 'w') as filehandle:  
+    for listitem in cos_zeniths:
+        filehandle.write('%f\n' % listitem) 
+with open('/data/user/smccarthy/gen2_files/weighting/pDOM/final/area_eff.txt', 'w') as filehandle:  
+    for listitem in area_eff:
+        filehandle.write('%f\n' % listitem) 
 with open('/data/user/smccarthy/gen2_files/weighting/pDOM/final/weights.txt', 'w') as filehandle:  
     for listitem in weights:
         filehandle.write('%f\n' % listitem)
-
+        
+        
 #mDOMdata
 SPE_data = []
 weights = []
+energies =[]
+cos_zeniths = []
+area_eff = []
 mDOM_files = glob.glob('/data/user/msilva/metaprojects/Gen2-Scripts/RECO_SPEs/MCReco_mDOM.i3.bz2')
 for item in mDOM_files:
     f = dataio.I3File(item)
@@ -53,24 +72,35 @@ for item in mDOM_files:
             flux = model.flux(MuonGun.depth(muon.pos.z), np.cos(muon.dir.zenith), 1)*model.energy(MuonGun.depth(muon.pos.z), np.cos(muon.dir.zenith), 1, 0, muon.energy)
             weight = flux*frame['MuonEffectiveArea'].value
             weights.append(weight)
-            energy = frame["MCMuon"].energy
-            cos_zenith = np.cos(frame["MCMuon"].dir.zenith)
-            eff_area = frame["MuonEffectiveArea"].value
+            energies.append(weight*frame["MCMuon"].energy)
+            cos_zeniths.append(weight*np.cos(frame["MCMuon"].dir.zenith))
+            area_eff.append(frame["MuonEffectiveArea"].value)
             for DOM, MCPE in frame["I3RecoPulseSeriesMapGen2"]:
                 for SPE in MCPE:
-                    DOMstr = str((DOM[0])+ (DOM[1]))
-                    DOMs = int(DOMstr)
-                    data = [DOMs,SPE.charge, SPE.time, weight, cos_zenith, eff_area, energy]
-                    SPE_data.append(data)
-file = '/data/user/smccarthy/gen2_files/weighting/mDOM/final/SPEarray.txt'
-np.savetxt(file,SPE_data) 
+                    data = [str(DOM[0]) +',' + str(DOM[1]),SPE.charge, SPE.time]
+                    np.concatenate((SPE_data, data))
+with open('/data/user/smccarthy/gen2_files/weighting/mDOM/final/SPEarray.txt', 'w') as filehandle:  
+    for listitem in SPE_data:
+        filehandle.write('%f\n' % listitem)  
+with open('/data/user/smccarthy/gen2_files/weighting/mDOM/final/energies.txt', 'w') as filehandle:  
+    for listitem in energies:
+        filehandle.write('%f\n' % listitem) 
+with open('/data/user/smccarthy/gen2_files/weighting/mDOM/final/cos_zeniths.txt', 'w') as filehandle:  
+    for listitem in cos_zeniths:
+        filehandle.write('%f\n' % listitem) 
+with open('/data/user/smccarthy/gen2_files/weighting/mDOM/final/area_eff.txt', 'w') as filehandle:  
+    for listitem in area_eff:
+        filehandle.write('%f\n' % listitem) 
 with open('/data/user/smccarthy/gen2_files/weighting/mDOM/final/weights.txt', 'w') as filehandle:  
     for listitem in weights:
-        filehandle.write('%f\n' % listitem)
-
+        filehandle.write('%f\n' % listitem) 
+        
 #DEggdata
 SPE_data = []
 weights = []
+energies =[]
+cos_zeniths = []
+area_eff = []
 pDOM_files = glob.glob('/data/user/msilva/metaprojects/Gen2-Scripts/RECO_SPEs/MCReco_DEgg.i3.bz2')
 for item in pDOM_files:
     f = dataio.I3File(item)
@@ -81,17 +111,25 @@ for item in pDOM_files:
             flux = model.flux(MuonGun.depth(muon.pos.z), np.cos(muon.dir.zenith), 1)*model.energy(MuonGun.depth(muon.pos.z), np.cos(muon.dir.zenith), 1, 0, muon.energy)
             weight = flux*frame['MuonEffectiveArea'].value
             weights.append(weight)
-            energy = frame["MCMuon"].energy
-            cos_zenith = np.cos(frame["MCMuon"].dir.zenith)
-            eff_area = frame["MuonEffectiveArea"].value
+            energies.append(weight*frame["MCMuon"].energy)
+            cos_zeniths.append(weight*np.cos(frame["MCMuon"].dir.zenith))
+            area_eff.append(frame["MuonEffectiveArea"].value)
             for DOM, MCPE in frame["I3RecoPulseSeriesMapGen2"]:
                 for SPE in MCPE:
-                    DOMstr = str((DOM[0])+ (DOM[1]))
-                    DOMs = int(DOMstr)
-                    data = [DOMs,SPE.charge, SPE.time, weight, cos_zenith, eff_area, energy]
-                    SPE_data.append(data)
-file = '/data/user/smccarthy/gen2_files/weighting/DEgg/final/SPEarray.txt'
-np.savetxt(file,SPE_data) 
+                    data = [str(DOM[0]) +',' + str(DOM[1]),SPE.charge, SPE.time]
+                    np.concatenate((SPE_data, data))
+with open('/data/user/smccarthy/gen2_files/weighting/DEgg/final/SPEarray.txt', 'w') as filehandle:  
+    for listitem in SPE_data:
+        filehandle.write('%f\n' % listitem)  
+with open('/data/user/smccarthy/gen2_files/weighting/DEgg/final/energies.txt', 'w') as filehandle:  
+    for listitem in energies:
+        filehandle.write('%f\n' % listitem) 
+with open('/data/user/smccarthy/gen2_files/weighting/DEgg/final/cos_zeniths.txt', 'w') as filehandle:  
+    for listitem in cos_zeniths:
+        filehandle.write('%f\n' % listitem) 
+with open('/data/user/smccarthy/gen2_files/weighting/DEgg/final/area_eff.txt', 'w') as filehandle:  
+    for listitem in area_eff:
+        filehandle.write('%f\n' % listitem) 
 with open('/data/user/smccarthy/gen2_files/weighting/DEgg/final/weights.txt', 'w') as filehandle:  
     for listitem in weights:
-        filehandle.write('%f\n' % listitem)
+        filehandle.write('%f\n' % listitem) 
